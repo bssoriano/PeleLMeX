ed84cdedc65d28d910ae1ed14be7a0b1fbff5f74
Bruno Soriano <bsouzas@sandia.gov>
2025-05-15 23:29:28 -0700

    Case set up working with EBinflow in 2D

--- Exec/Production/CrossFlowWithInjection-TURNS/pelelmex_prob.cpp
+++ Exec/Production/CrossFlowWithInjection-TURNS/pelelmex_prob.cpp
@@ -6,18 +6,22 @@ PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
 {
   amrex::ParmParse pp("prob");
 
-  pp.query("injection_diam", prob_parm->injection_diam);
+  pp.query("jet_radius", prob_parm->jet_radius);
   pp.query("T_mean", prob_parm->T_mean);
   pp.query("P_mean", prob_parm->P_mean);
   pp.query("meanFlowDir", prob_parm->meanFlowDir);
   pp.query("meanFlowMag", prob_parm->meanFlowMag);
+  pp.query("jet_vel",prob_parm->jet_vel);
+  pp.query("jet_vel",prob_parm->jet_vel);
+  pp.query("jet_T",prob_parm->jet_T);
+  pp.query("jet_Yfuel",prob_parm->jet_Yfuel);
 
   amrex::Vector<amrex::Real> local_inject_loc(
     AMREX_SPACEDIM, std::numeric_limits<amrex::Real>::lowest());
-  pp.queryarr("inject_loc", local_inject_loc, 0, AMREX_SPACEDIM);
+  pp.queryarr("jet_loc", local_inject_loc, 0, AMREX_SPACEDIM);
 
   for (int i = 0; i < AMREX_SPACEDIM; i++) {
-    PeleLM::prob_parm->nject_loc[i] = local_inject_loc[i];
+    PeleLM::prob_parm->jet_loc[i] = local_inject_loc[i];
   }
 
   // if (!m_incompressible) {
