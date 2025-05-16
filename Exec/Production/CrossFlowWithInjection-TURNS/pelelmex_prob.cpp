aa410e4c1fcafba99feab0644ccb59f1c1bc2407
BSoriano <bsouzas@sandia.gov>
2025-05-15 21:33:52 -0700

    Work in progress: implementation of flare flame case

--- Exec/Production/CrossFlowWithInjection-TURNS/pelelmex_prob.cpp
+++ Exec/Production/CrossFlowWithInjection-TURNS/pelelmex_prob.cpp
@@ -6,10 +6,19 @@ PeleLM::readProbParm() // NOLINT(readability-make-member-function-const)
 {
   amrex::ParmParse pp("prob");
 
+  pp.query("injection_diam", prob_parm->injection_diam);
   pp.query("T_mean", prob_parm->T_mean);
   pp.query("P_mean", prob_parm->P_mean);
   pp.query("meanFlowDir", prob_parm->meanFlowDir);
   pp.query("meanFlowMag", prob_parm->meanFlowMag);
+
+  amrex::Vector<amrex::Real> local_inject_loc(
+    AMREX_SPACEDIM, std::numeric_limits<amrex::Real>::lowest());
+  pp.queryarr("inject_loc", local_inject_loc, 0, AMREX_SPACEDIM);
+
+  for (int i = 0; i < AMREX_SPACEDIM; i++) {
+    PeleLM::prob_parm->nject_loc[i] = local_inject_loc[i];
+  }
 
   // if (!m_incompressible) {
   //    auto& trans_parm = PeleLM::trans_parms.host_parm();
